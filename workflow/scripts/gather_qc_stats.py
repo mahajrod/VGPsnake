8f72a474482ae1b74110d6c6ad38c5faade5a49b
mahajrod <mahajrod@gmail.com>
2023-05-15 00:39:57 +0200

    fixes

--- workflow/scripts/gather_qc_stats.py
+++ workflow/scripts/gather_qc_stats.py
@@ -11,6 +11,7 @@ from RouToolPa.Parsers.BUSCO import BUSCOtable
 def read_gene_string_from_busco_summary(in_file):
     with open(in_file, "r") as in_fd:
         for line in in_fd:
+            print(line)
             if "***** Results: *****" in line:
                 break
         in_fd.readline()
